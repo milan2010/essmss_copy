0eb9169276b16ceb680192dcd801e38791721b9b
Gustavo Yauny <gustavo.yauny@gmail.com>
2017-01-27 09:39:58 +0100

    [Add] Contact Person page

--- src/app/pages/personal/contactperson/contactperson.components.ts
+++ src/app/pages/personal/contactperson/contactperson.components.ts
@@ -6,12 +6,49 @@ import {NavController} from 'ionic-angular';
   templateUrl: 'contactperson.html'
 })
 export class ContactpersonPage {
+  contacts: Array<Object> = [];
 
   constructor(public navCtrl: NavController) {
-  }
+    this.contacts = [
+      {
+        "name" : "Olaf Muller",
+        "role" : "Meister",
+        "email" : "omuller@email.com",
+        "phone" : "1234567890"
+      },
+      {
+        "name" : "Josef Hermann",
+        "role" : "Teamsprecher",
+        "email" : "jhermann@email.com",
+        "phone" : "1234567890"
+      },
+      {
+        "name" : "Wilhelm Ackert",
+        "role" : "Monteur Maschine 1",
+        "email" : "olaf@email.com",
+        "phone" : "1234567890"
+      },
+      {
+        "name" : "Sebastian Saal",
+        "role" : "Monteur Maschine 2",
+        "email" : "ssaal@email.com",
+        "phone" : "1234567890"
+      },
+      {
+        "name" : "Christian Harnisch",
+        "role" : "Monteur Maschine 3",
+        "email" : "ssaal@email.com",
+        "phone" : "1234567890"
+      },
+      {
+        "name" : "John Albrecht",
+        "role" : "Monteur Maschine 4",
+        "email" : "ssaal@email.com",
+        "phone" : "1234567890"
+      }
+    ]
 
-  ionViewDidLoad() {
-    console.log('Hello ContactpersonPage Page');
+
   }
 
 }
