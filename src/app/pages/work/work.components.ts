07b01f2b697b3e0b80268401e389789a015474de
michaelkraus-bgo <michael.kraus@boldlygo.de>
2017-02-01 16:54:45 +0100

    Added page for canteen

--- src/app/pages/work/work.components.ts
+++ src/app/pages/work/work.components.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {PresswerkPage} from "./presswerk/presswerk.components";
+import {CanteenPage} from "./canteen/canteen.component";
 
 @Component({
   selector: 'page-work',
@@ -10,13 +11,13 @@ export class WorkPage {
 
   constructor(public nav: NavController) {
   }
-  
+
   goToMachine() {
     this.nav.push(PresswerkPage);
   }
-  
+
   goToKantine() {
-//    this.nav.push(KantinePage);
+    this.nav.push(CanteenPage);
   }
 
 }
