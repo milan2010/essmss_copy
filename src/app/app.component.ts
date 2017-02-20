0efe9033d2ef1b03157e0fc7387f2743b39b5183
Gustavo Yauny <gustavo.yauny@gmail.com>
2017-02-20 14:58:00 +0100

    vwx-51:Test 3D-touch functionality and include it exemplary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import {Component} from '@angular/core';
-import {Platform} from 'ionic-angular';
-import { TranslateService } from 'ng2-translate';
-import {StatusBar, Splashscreen} from 'ionic-native';
+import {Component, ViewChild} from '@angular/core';
+import {Platform, NavController} from 'ionic-angular';
+import {TranslateService} from 'ng2-translate';
+import {StatusBar, Splashscreen, ThreeDeeTouch} from 'ionic-native';
 
 import {LoginPage} from './pages/login/login.component';
 import {TabsPage} from './pages/tabs/tabs.component';
@@ -9,9 +9,13 @@ import {TutorialPage} from './pages/tutorial/tutorial.component';
 
 import {UserService} from './services/user.service';
 import {PersonService} from "./services/person.service";
-import { SettingsService } from './pages/settings/settings.service';
-import { AuthorizationService } from './services/authorization.service';
-import { StorageService } from './services/storage.service';
+import {SettingsService} from './pages/settings/settings.service';
+import {AuthorizationService} from './services/authorization.service';
+import {StorageService} from './services/storage.service';
+import {TeamPage} from "./pages/team/team.components";
+import {ChatPage} from "./pages/chat/chat.component";
+import {WorkPage} from "./pages/work/work.components";
+import {WorkingtimePage} from "./pages/personal/workingtime/workingtime.components";
 
 @Component({
   templateUrl: 'app.html',
@@ -19,33 +23,34 @@ import { StorageService } from './services/storage.service';
 })
 
 export class MyApp {
+  @ViewChild('myNav') nav: NavController;
   rootPage: any;
   chosenTheme: string;
 
   constructor(private platform: Platform, private storageService: StorageService, private userService: UserService, private translate: TranslateService, private settingsService: SettingsService, private authorizationService: AuthorizationService) {
     this.storageService.get(StorageService.TUTORIAL_SHOWN)
-    .then((hasSeenTutorial) => {
+      .then((hasSeenTutorial) => {
 
-      if(hasSeenTutorial){
-        if(this.userService.isLoggedIn()){
-          this.rootPage = TabsPage;
+        if (hasSeenTutorial) {
+          if (this.userService.isLoggedIn()) {
+            this.rootPage = TabsPage;
+          } else {
+            this.rootPage = LoginPage;
+          }
         } else {
-          this.rootPage = LoginPage;
+          if (this.userService.isLoggedIn()) {
+            this.rootPage = TabsPage;
+          } else {
+            this.rootPage = TutorialPage;
+          }
         }
-      } else {
-        if(this.userService.isLoggedIn()){
-          this.rootPage = TabsPage;
-        } else {
-          this.rootPage = TutorialPage;
-        }
-      }
 
-    });
+      });
 
     this.platformReady();
   }
 
-  platformReady(){
+  platformReady() {
     this.platform.ready().then(() => {
       // Subscribe to theme changes and set a default chosen theme
       this.settingsService.getTheme().subscribe(val => this.chosenTheme = val);
@@ -57,6 +62,62 @@ export class MyApp {
       this.translate.setDefaultLang(SettingsService.DEFAULT_LANGUAGE);
       // the lang to use, if the lang isn't available, it will use the current loader to get them
       this.translate.use(SettingsService.DEFAULT_LANGUAGE);
+      this.prepare3dTouch();
     });
+  }
+
+  prepare3dTouch() {
+    if (!this.platform.is('cordova')) {
+      return;
+    }
+
+    ThreeDeeTouch.isAvailable()
+      .then(isAvailable => {
+        console.log("3D Touch available? " + isAvailable);
+        if (isAvailable) {
+          ThreeDeeTouch.configureQuickActions([
+            {
+              type: 'team',
+              title: 'Mein Team',
+              iconType: 'Contact'
+            },
+            {
+              type: 'chat',
+              title: 'Chat',
+              iconType: 'Message'
+            },
+            {
+              type: 'work',
+              title: 'Meine Arbeit',
+              iconType: 'Task'
+            },
+            {
+              type: 'workingtime',
+              title: 'Arbeitszeit',
+              iconType: 'Date'
+            }
+          ]);
+
+          ThreeDeeTouch.onHomeIconPressed().subscribe(
+            (payload) => {
+              console.log(payload.type);
+              switch (payload.type) {
+                case 'team':
+                  this.nav.push(TeamPage);
+                  break;
+                case 'chat':
+                  this.nav.push(ChatPage);
+                  break;
+                case 'work':
+                  this.nav.push(WorkPage);
+                  break;
+                case 'workingtime':
+                  this.nav.push(WorkingtimePage);
+                  break;
+              }
+            }
+          )
+        }
+      });
   }
 }
