88aed9877985a798b029302f86c7154b1bec3e29
Kirsch, Thore (K-SIPB-2/4) <thore.kirsch@volkswagen.de>
2017-06-29 08:21:13 +0200

    Switch from mock data to jive backend

--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -30,21 +30,11 @@ timer = Observable.timer(5000,1000);
   }
 
   ionViewDidLoad() {
-
-      this.discussions.push(new Discussion(205, new Date(), 23,1,"Standortsymposium in BS", new Content("Der Zukunftspakt gibt den Fahrplan für den Umbau des Autobauers VW vor. Was sich durch die Vereinbarung im Werk Braunschweig verändert, welche Probleme bewältigt werden müssen und welche Chancen sich ergeben, erläutern Werkleiter Otto Joos und Betriebsratschef Uwe Fritsch im Interview mit Armin Maus und Andreas Schweiger."), null, null,false,false,false,'assets/img/bv_BS.jpeg',['assets/img/bv_BS.jpeg'],[],true));
-      this.discussions.push(new Discussion(205, new Date(), 83,2,"Halle 31 in TPM Stufe 3", new Content("In der Lenkungsmontage der Halle 31 sind die Bereiche MQB (Modularer Quer Baukasten) und RC-EPS (rack concentric electric power steering) nun komplett in der TPM-Stufe 3."), null, null,false,false,false,'assets/img/tpm.jpg',['assets/img/tpm.jpg'],[],true));
-      this.discussions.push(new Discussion(205, new Date(), 3,4,"Der neue E-Motor", 
-      new Content("Die Produktion des neuen Elektromotors startet in der Komponente."), null, null,false,false,false,'assets/img/emotor.jpg',
-      ['assets/img/emotor.jpg', 'assets/img/elektrowagen.jpg'],['Testkommentar','Testkommentar2'],true));
-
-      this.discussionsVIP.push(new Discussion(205, new Date(), 83,2,"Neue Struktur", new Content("Thomas Schmall, bisher Präsident und CEO von Volkswagen do Brasil, wird mit Wirkung zum 1. Januar 2015 Mitglied des Markenvorstands Volkswagen für den Geschäftsbereich Komponente. "), null, null,false,false,false,'assets/img/schmall.jpg',['assets/img/vw.jpg','assets/img/elektrowagen.jpg'],[],false));
-
-      //this.jiveService.getDiscussions().subscribe(x => {
-      //  console.log(x);
-      //  x.forEach(d => this.discussions.push(d));
-      //  this.updateFilter();
-      //});
-      //this.discussionsVIP.push(new Discussion(205, new Date(), 83,2,"Neue Struktur", new Content("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."), null, null,false,false,false,'assets/img/schmall.jpg',['assets/img/elektrowagen.jpg','assets/img/emotor.jpg'],[],false));
+      this.jiveService.getDiscussions().subscribe(x => {
+        x.forEach(d => this.discussions.push(d));
+        this.updateFilter();
+      });
+      this.discussionsVIP.push(new Discussion(205, new Date(), 83,2,"Neue Struktur", new Content("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."), null, null,false,false,false,'assets/img/schmall.jpg',['assets/img/elektrowagen.jpg','assets/img/emotor.jpg'],[],false));
       this.updateFilter();
 
     this.timer.subscribe(t=> {
